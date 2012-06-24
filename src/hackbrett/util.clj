f9b370a69fc0c8871c1656c1cf1fec56f6cec2b0
Martin Sander <forke@uni-bonn.de>
2012-06-24 18:35:26 +0200

    some updates for the useless test

--- src/hackbrett/util.clj
+++ src/hackbrett/util.clj
@@ -1,26 +1,24 @@
-(ns hackbrett.util)
+(ns hackbrett.util
+  (:use clojure.pprint))
 
 (defn update-in-wildcard
   "like update-in, but also accepts :_ as a wildcard key.
   If :_ is provided, ALL keys on that level are handled.
   Warning: might explode for big data structures"
-  ([hmap [key & keys] f & args]
-   (if (= key :_) ;; TODO better placeholder?
-     (into {}
-           (if keys
-             (map
-               (fn [[key val]]
-                 [key
-                  (apply update-in
-                         val
-                         keys f args)]) 
-               hmap)
-             (map
-               (fn [[key val]]
-                 [key (apply f val args)]) 
-               hmap))) 
-     (if keys
-       (assoc hmap key
-              (apply update-in-wildcard (get hmap key) keys f args))
-       (assoc hmap key
-              (apply f (get hmap key) args))))))
+  [hmap [key & ks] f & args]
+  (if (= key :_) ;; TODO better placeholder?
+    (into {}
+          (if ks
+            (map
+              (fn [[k v]]
+                [k (apply update-in-wildcard v ks f args)])
+              hmap)
+            (map
+              (fn [[k v]]
+                [k (apply f v args)])
+              hmap)))
+    (if ks
+      (assoc hmap key (apply update-in-wildcard (get hmap key) ks f args))
+      (do
+        (pprint [hmap key args])
+        (assoc hmap key (apply f (get hmap key) args))))))
--- test/hackbrett/test/util.clj
+++ test/hackbrett/test/util.clj
@@ -1,5 +1,42 @@
 (ns hackbrett.test.util
   (:use [hackbrett.util])
-  (:use [clojure.test]))
+  (:use [midje.sweet]))
+
+(tabular
+  (fact "update without wildcards behaves like update-in"
+        (apply update-in-wildcard ?args) => (apply update-in ?args))
+  ?args
+  [{} [] identity]
+  [{:a 1} [:a] identity]
+  [{:a 1} [:a] (constantly 2)]
+  [{:a 1 :b 2} [:a] (constantly 2)]
+  [{:a {:b {:c 2}}} [:a :b :c] inc]
+  )
 
 
+(fact "should throw the same exceptions as update-in"
+  (update-in-wildcard
+    {:a {:b 1}} [:a :a] inc) => (throws NullPointerException))
+
+(fact "incompatible levels throw (compatible with update-in)"
+      (update-in-wildcard {:a {:b 1}} [:_ :a] inc)
+      =>                  (throws NullPointerException))
+
+(fact "when levels are missing, hashmaps are created"
+      (update-in-wildcard {} [:a :b] (constantly 1)) => {:a {:b 1}}
+      (update-in-wildcard {:a {}} [:_ :b] (constantly 1)) => {:a {:b 1}})
+
+(fact "missing levels with wildcard expressions are not created (i.e. map is not updated)"
+      (update-in-wildcard {} [:_ :a] (constantly 1)) => {})
+
+(fact "when used with a wildcard, all the keys on that level are updated"
+      (update-in-wildcard {:a 1 :b 2} [:_] inc)
+      =>                  {:a 2 :b 3}
+      (update-in-wildcard {:a {:a 1} :b {:a 1}} [:_ :a] inc)
+      =>                  {:a {:a 2} :b {:a 2}}
+      (update-in-wildcard {:a {:b 1} :b {:a 1}} [:_ :_] inc)
+      =>                  {:a {:b 2} :b {:a 2}}
+      (update-in-wildcard {:a {:b 1} :b {:a 1}} [:b :_] inc)
+      =>                  {:a {:b 1} :b {:a 2}}
+      (update-in-wildcard {:a {:b 1} :b {:a 1 :c 2}} [:b :_] inc)
+      =>                  {:a {:b 1} :b {:a 2 :c 3}})
