583182df521e2964682c6f17c408a5e17c606dd8
Martin Sander <forke@uni-bonn.de>
2012-07-08 17:49:15 +0200

    fresh start: "midi-pad only" works

--- src/hackbrett/mapping.clj
+++ src/hackbrett/mapping.clj
@@ -1,10 +1,18 @@
 (ns hackbrett.mapping
+  (:use hackbrett.util)
   (:use clojure.pprint)
-  (:require [somnium.congomongo :as mongo] ) ;; TODO consider moving to mongo ns
+  (:require [somnium.congomongo :as mongo]) ;; TODO consider moving to mongo ns
   (:use [clojure.tools.logging :only [error]])
  ; (:use [clojure.java.io :only [copy output-stream]])
   )
 
-(defn get-samples [])
+(defn get-samples []
+  (mongo/fetch :samples))
 
-(defn get-sample-filename [note])
+(defn get-sample-filename [note]
+  (let [id-filename (:id-filename
+                      (mongo/fetch-one :bindings
+                                       :where {:midi-key note}))
+        sample (mongo/fetch-one :samples
+                                :where {:id-filename id-filename})]
+    (:real-filename sample)))
